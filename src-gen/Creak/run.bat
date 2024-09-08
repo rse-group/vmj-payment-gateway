5b0752b3124e1580f13fa1574dfa41237ccfbfef
sulthanftr <sulthanftr@gmail.com>
2024-09-09 00:16:49 +0700

    modify .gitignore

--- src-gen/Creak/run.bat
+++
@@ -1,4 +0,0 @@
-echo SELECT 'CREATE DATABASE paymentgateway_product_creak' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'paymentgateway_product_creak') \gexec | psql "postgresql://:@localhost"
-for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://:@localhost/paymentgateway_product_creak"
-
-java -cp paymentgateway.product.creak --module-path paymentgateway.product.creak -m paymentgateway.product.creak
