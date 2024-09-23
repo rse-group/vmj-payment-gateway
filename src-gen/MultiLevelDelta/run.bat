e50f5d22d0f6b9ae2628a98ad73fe1e9d1c9c071
sulthanftr <sulthanftr@gmail.com>
2024-09-23 07:43:08 +0700

    clean generated directories

--- src-gen/MultiLevelDelta/run.bat
+++
@@ -1,4 +0,0 @@
-echo SELECT 'CREATE DATABASE paymentgateway_product_multileveldelta' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'paymentgateway_product_multileveldelta') \gexec | psql "postgresql://postgres:postgres@localhost"
-for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:postgres@localhost/paymentgateway_product_multileveldelta"
-
-java -cp paymentgateway.product.multileveldelta --module-path paymentgateway.product.multileveldelta -m paymentgateway.product.multileveldelta
