1ce08a5c6428107873ae994eff0a923d3c897318
dhafinn <dhafin.raditya@ui.ac.id>
2024-11-15 00:49:47 +0700

    fix: credit card flow by shooting token first

--- modules/paymentgateway.payment.creditcard/paymentgateway/payment/creditcard/service/PaymentServiceImpl.java
+++ modules/paymentgateway.payment.creditcard/paymentgateway/payment/creditcard/service/PaymentServiceImpl.java
@@ -42,34 +42,102 @@ public class PaymentServiceImpl extends PaymentServiceDecorator {
 		return creditCardTransaction;
 	}
 
+//	public Map<String, Object> sendTransaction(Map<String, Object> requestBody) {
+//		String vendorName = (String) requestBody.get("vendor_name");
+//
+//		Config config = ConfigFactory.createConfig(vendorName, ConfigFactory.createConfig("paymentgateway.config.core.ConfigImpl"));
+//		
+//		Gson gson = new Gson();
+//		Map<String, Object> requestMap = config.getCreditCardRequestBody(requestBody);
+//		int id = ((Integer) requestMap.get("id")).intValue();
+//		requestMap.remove("id");
+//		String requestString = config.getRequestString(requestMap);
+//		String configUrl = config.getProductEnv("CreditCard");
+//		System.out.println(configUrl);
+//		HashMap<String, String> headerParams = config.getHeaderParams();
+//		HttpClient client = HttpClient.newHttpClient();
+//		HttpRequest request = (config.getBuilder(HttpRequest.newBuilder(),headerParams))
+//				.uri(URI.create(configUrl))
+//				.POST(HttpRequest.BodyPublishers.ofString(requestString))
+//				.build();
+//
+//		Map<String, Object> responseMap = new HashMap<>();
+//		try {
+//			HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//			String rawResponse = response.body().toString();
+//			System.out.println("rawResponse " + rawResponse);
+//			responseMap = config.getCreditCardResponse(rawResponse, id);
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
+//
+//		return responseMap;
+//	}
+	
 	public Map<String, Object> sendTransaction(Map<String, Object> requestBody) {
-		String vendorName = (String) requestBody.get("vendor_name");
+	    String vendorName = (String) requestBody.get("vendor_name");
+	    String cardNumber = (String) requestBody.get("card_number");
+	    String cardExpMonth = (String) requestBody.get("card_exp_month");
+	    String cardExpYear = (String) requestBody.get("card_exp_year");
+	    String cardCVV = (String) requestBody.get("card_cvv");
 
-		Config config = ConfigFactory.createConfig(vendorName, ConfigFactory.createConfig("paymentgateway.config.core.ConfigImpl"));
-		
-		Gson gson = new Gson();
-		Map<String, Object> requestMap = config.getCreditCardRequestBody(requestBody);
-		int id = ((Integer) requestMap.get("id")).intValue();
-		requestMap.remove("id");
-		String requestString = config.getRequestString(requestMap);
-		String configUrl = config.getProductEnv("CreditCard");
-		HashMap<String, String> headerParams = config.getHeaderParams();
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = (config.getBuilder(HttpRequest.newBuilder(),headerParams))
-				.uri(URI.create(configUrl))
-				.POST(HttpRequest.BodyPublishers.ofString(requestString))
-				.build();
+	    Config config = ConfigFactory.createConfig(vendorName, ConfigFactory.createConfig("paymentgateway.config.core.ConfigImpl"));
+	    Gson gson = new Gson();
+	    
+	    // Step 1: Get credit card token
+	    String tokenUrl = config.getProductEnv("CreditCardToken");;
+	    
+	    String tokenRequestUrl = tokenUrl + 
+	                             "&card_number=" + cardNumber + 
+	                             "&card_exp_month=" + cardExpMonth + 
+	                             "&card_exp_year=" + cardExpYear + 
+	                             "&card_cvv=" + cardCVV;
+	    
+	    HttpRequest tokenRequest = HttpRequest.newBuilder()
+	        .uri(URI.create(tokenRequestUrl))
+	        .header("Accept", "application/json")
+	        .header("Content-Type", "application/json")
+	        .build();
+	    
+	    String tokenId = null;
+	    try {
+	        HttpResponse<String> tokenResponse = HttpClient.newHttpClient().send(tokenRequest, HttpResponse.BodyHandlers.ofString());
+	        Map<String, Object> tokenResponseMap = gson.fromJson(tokenResponse.body(), Map.class);
+	        tokenId = (String) tokenResponseMap.get("token_id");
+	        System.out.println(tokenId);
+	    } catch (Exception e) {
+	        System.out.println("Failed to get token: " + e.getMessage());
+	        return Map.of("error", "Token retrieval failed");
+	    }
+	    
+	    // Step 2: Send transaction request
+	    Map<String, Object> requestMap = config.getCreditCardRequestBody(requestBody);
+	    int id = ((Integer) requestMap.get("id")).intValue();
+	    requestMap.remove("id");
+	    requestMap.put("credit_card", Map.of("token_id", tokenId, "authentication", false));
+	    requestMap.put("payment_type", "credit_card");
 
-		Map<String, Object> responseMap = new HashMap<>();
-		try {
-			HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
-			String rawResponse = response.body().toString();
-			System.out.println("rawResponse " + rawResponse);
-			responseMap = config.getCreditCardResponse(rawResponse, id);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+	    String requestString = gson.toJson(requestMap);
+	    String configUrl = config.getProductEnv("CreditCard");
+	    System.out.println(configUrl);
 
-		return responseMap;
+	    HashMap<String, String> headerParams = config.getHeaderParams();
+	    HttpRequest transactionRequest = (config.getBuilder(HttpRequest.newBuilder(), headerParams))
+	        .uri(URI.create(configUrl))
+	        .POST(HttpRequest.BodyPublishers.ofString(requestString))
+	        .build();
+
+	    Map<String, Object> responseMap = new HashMap<>();
+	    try {
+	        HttpResponse<String> response = HttpClient.newHttpClient().send(transactionRequest, HttpResponse.BodyHandlers.ofString());
+	        String rawResponse = response.body();
+	        System.out.println("Transaction Response: " + rawResponse);
+	        responseMap = config.getCreditCardResponse(rawResponse, id);
+	    } catch (Exception e) {
+	        System.out.println("Transaction failed: " + e.getMessage());
+	    }
+
+	    return responseMap;
 	}
+
 }
