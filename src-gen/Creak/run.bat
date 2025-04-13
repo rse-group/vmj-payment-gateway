0d498db9a65ac31e0ca9a6e5c116a4d4216ea119
Gibrano Derenz <gibranoderenz123@gmail.com>
2025-04-13 19:25:22 +0700

    chore: remove modules-gen folder and src-gen folder

--- src-gen/Creak/run.bat
+++
@@ -1,4 +0,0 @@
-echo SELECT 'CREATE DATABASE paymentgateway_product_creak' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'paymentgateway_product_creak') \gexec | psql "postgresql://postgres:admin@localhost"
-for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:admin@localhost/paymentgateway_product_creak"
-
-java -cp paymentgateway.product.creak --module-path paymentgateway.product.creak -m paymentgateway.product.creak
