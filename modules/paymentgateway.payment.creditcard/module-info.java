ee16c61f1ed9a394245758efc0ddf6ea0065197f
eruzetaien <muhammad.ruzain@ui.ac.id>
2025-04-16 08:26:35 +0700

    fix: change order of requires descriptive in module info and add src directory to gitignore

--- modules/paymentgateway.payment.creditcard/module-info.java
+++ modules/paymentgateway.payment.creditcard/module-info.java
@@ -1,11 +1,10 @@
 module paymentgateway.payment.creditcard {
 	requires paymentgateway.payment.core;
     exports paymentgateway.payment.creditcard;
-
-	requires vmj.routing.route;
-	requires paymentgateway.config.core;
+    requires paymentgateway.config.core;
 	requires paymentgateway.config.midtrans;
-
+	
+	requires vmj.routing.route;
 	requires vmj.hibernate.integrator;
 	requires vmj.auth;
 	requires java.logging;
