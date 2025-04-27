2808afd2d6c94d3596b85925c4006422eaf9bd9b
Gibrano Derenz <gibranoderenz123@gmail.com>
2025-04-27 12:07:05 +0700

    fix: update CheckPaymentStatusRequestBody

--- modules/paymentgateway.config.core/dto/CheckPaymentStatusRequestBody.java
+++ modules/paymentgateway.config.core/dto/CheckPaymentStatusRequestBody.java
@@ -1,5 +1,7 @@
 package paymentgateway.config.core;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -7,8 +9,9 @@ import javax.validation.constraints.Pattern;
 public class CheckPaymentStatusRequestBody {
 	@NotEmpty(message = "vendor_name must be specified")
 	@Pattern(regexp = "Flip|Midtrans|Oy|Xendit", message = "Vendor name is invalid; Valid values: Flip, Midtrans, Oy, Xendit")
+	@JsonProperties("vendor_name")
 	public String vendorName;
-	
+
 	@NotNull(message = "id must be specified")
 	public int id;
 }
