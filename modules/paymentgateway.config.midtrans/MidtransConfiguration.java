8debf95a4685670157137cb8e8d1a7f8d07d7d42
Gibrano Derenz <gibranoderenz123@gmail.com>
2025-05-27 16:51:46 +0700

    fix: handle error from vendor on getVirtualAccountResponse in MidtransConfiguration

--- modules/paymentgateway.config.midtrans/MidtransConfiguration.java
+++ modules/paymentgateway.config.midtrans/MidtransConfiguration.java
@@ -331,7 +331,7 @@ public class MidtransConfiguration extends ConfigDecorator{
         Map<String, Object> rawResponseMap = gson.fromJson(rawResponse, mapType);
 
         String statusCode = (String) rawResponseMap.get("status_code");
-        if (!statusCode.equals("200") || !statusCode.equals("201")) {
+        if (!statusCode.equals("200") && !statusCode.equals("201")) {
             String errorMessageString = (String) rawResponseMap.get("status_message");
             throw new BadRequestException(errorMessageString);
         }
